67e755ea52e43b59e80986d044217b8afc05f347
Mark Blashki <markblashki1@gmail.com>
2022-04-05 23:56:42 +1000

    add: allow for backend to deliver messages to each seperate client, instead of only one message per client

--- backend/studio_dashboard/dashboard_api/views.py
+++ backend/studio_dashboard/dashboard_api/views.py
@@ -1,13 +1,13 @@
 import datetime
 import json
+from pyclbr import Function
 import threading
 import time
 from django.http import HttpResponse, StreamingHttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
 from django.template import loader
 import urllib
 from django.conf import settings
-
-messageQueue = []
 
 datetime_timer_running = False
 weather_timer_running = False
@@ -19,9 +19,51 @@ class Message:
   def json(self):
     return json.dumps(self, default=lambda o: o.__dict__)
 
+
+class Subscription:
+  def __init__(self):
+    self.queue = []
+    self.messageAvailable = False
+  
+  def add(self, message:Message):
+    self.queue.append(message)
+    self.messageAvailable = True
+  
+  def pop(self):
+    msg = self.queue.pop()
+    
+    if(len(self.queue) == 0):
+      self.messageAvailable = False
+    
+    return msg
+
+class MessageDeliverer:
+  def __init__(self):
+    self.subscribers = []
+  
+  def register(self, subscriber:Subscription):
+    self.subscribers.append(subscriber)
+    
+  def deregister(self, subscriber:Subscription):
+    self.subscribers.remove(subscriber)
+  
+  def notify(self, message:Message):
+    for subscriber in self.subscribers:
+      subscriber.add(message)
+
+class StreamingHttpResponseWithCallback(StreamingHttpResponse):
+  def setOnClose(self, func:Function):
+    self.onClose = func
+    
+  def close(self):
+    super(StreamingHttpResponseWithCallback, self).close()
+    self.onClose()
+
+message_deliverer = MessageDeliverer()
+
 def update_time():
   threading.Timer(30, update_time).start()
-  messageQueue.append(Message('date', {"date": datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}))
+  message_deliverer.notify(Message('date', {"date": datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}))
   
 def update_weather():
   # threading.Timer(60, update_weather).start()
@@ -30,7 +72,7 @@ def update_weather():
   weatherURL = f"https://api.openweathermap.org/data/2.5/onecall?lat=-37.79616776238214&lon=145.2922194203585&APPID={secret}&exclude=hourly,minutely"
   with urllib.request.urlopen(weatherURL) as url:
     data = json.loads(url.read().decode())
-    messageQueue.append(Message('weather', data))
+    message_deliverer.notify(Message('weather', data))
   
 
 def index(request):
@@ -53,42 +95,47 @@ def stream(request):
     weather_timer_running = True
     update_weather()
   
+  subscription = Subscription()
+  message_deliverer.register(subscription)
+  
   def event_stream():
     while True:
       time.sleep(0.25)
       
       # Push messages from queue
-      if len(messageQueue) > 0:
+      if subscription.messageAvailable:
         result = 'data: ['
-        while(len(messageQueue) > 0):
-          result += messageQueue.pop().json() + ','
+        while(subscription.messageAvailable):
+          result += subscription.pop().json() + ','
         
         result = result[:-1] + ']\n\n'
         yield result
   
-  return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
+  resp = StreamingHttpResponseWithCallback(event_stream(), content_type='text/event-stream')
+  resp.setOnClose(lambda: message_deliverer.deregister(subscription))
+  return resp
 
+@xframe_options_exempt
 def setLED(request):
   LED = request.GET["id"]
   state = str(request.GET["state"]) == "ON"
   
   msg = Message("pad", {'id': LED, 'state': state})
-  messageQueue.append(msg);
+  message_deliverer.notify(msg);
+  print(msg.json())
   
   response = HttpResponse(content=f"Set {msg.body['id']} to {msg.body['state']}", content_type='text/plain')
   response.status = 200
   return response;
 
+@xframe_options_exempt
 def alert(request):
   msg = Message("alert", {
     'text': request.GET["text"],
     'level': request.GET["level"],
     'timestamp': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
   })
-  messageQueue.append(msg);
-  
-  for message in messageQueue:
-    print(message.type)
+  message_deliverer.notify(msg);
   
   response = HttpResponse(content=msg.json(), content_type='text/plain')
   response.status = 200
